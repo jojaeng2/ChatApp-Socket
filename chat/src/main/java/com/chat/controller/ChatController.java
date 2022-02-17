a979963739bb69ee1f2ade077182a74777183b3e
jojaeng2 <ds4ouj@naver.com>
2022-02-17 10:27:24 +0900

    create ChatApp using by Socket

---
+++ chat/src/main/java/com/chat/controller/ChatController.java
@@ -0,0 +1,23 @@
+package com.chat.controller;
+
+import com.chat.domain.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+
+// import 생략...
+
+@RequiredArgsConstructor
+@Controller
+public class ChatController {
+
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.JOIN.equals(message.getType()))
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+    }
+}
